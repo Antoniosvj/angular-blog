299898c29b5dd01689565219d44fc5da3048fa0f
Antoniosvj <antoniosvj@hotmail.com>
2023-09-11 23:11:15 -0300

    personal blog committee held in angular for technology learning

---
+++ src/app/components/menu-big-car/menu-big-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'big-car',
+  templateUrl: './menu-big-car.component.html',
+  styleUrls: ['./menu-big-car.component.css']
+})
+export class MenuBigCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
